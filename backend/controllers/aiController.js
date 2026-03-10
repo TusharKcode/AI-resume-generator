6a97dc080957a6fe7d7efa55a80dd7fe4bd3a02d
Tushar Kumar <tkgoyal104@gmail.com>
2026-03-10 19:30:46 +0530

    API replaced

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,14 +1,28 @@
-import { generateResumeContent } from "../services/aiService.js";
+import { generateAiContent } from "../services/aiService.js";
+import db from "../config/db.js"
 
 export const generateAIContent = async(req, res) => {
     try {
-        const { section, userData } = req.body;
-        const aiText = await generateResumeContent(section, userData);
+        const { section, userData, resumeId } = req.body;
+
+        if(!resumeId){
+            return res.status(400).json({
+                message:"resumeId is required"
+            })
+        }
+
+        // Generate AI texts
+        const aiText = await generateAiContent(section, userData);
+
+        // save it into DB
+        const query = `INSERT INTO ai_generated_content (resume_id, section_name, ai_text) VALUES (?, ?, ?)`;
+        await db.execute(query, [resumeId, section, aiText])
 
         res.json({
             section, aiText
         });
     } catch (error) {
+        console.error(error);
         res.status(500).json({
             message: "AI generation failed",
             error: error.message
--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -1,22 +1,30 @@
-import OpenAI from "openai"
+import Groq from "groq-sdk";
+import dotenv from "dotenv"
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+dotenv.config();
+
+const groq = new Groq({
+    apiKey: process.env.GroqAI_API_KEY
 })
 
-export const generateResumeContent = async (section, userData) => {
+export const generateAiContent = async (section, userData) => {
     const prompt = `
         Generate a professional ${section} for a resume.
-        User Data: ${JSON.stringify(userData)}
-        Write it professionally for recruiters.
+        User Details: 
+        Name: ${userData.name}
+        Role: ${userData.role}
+        Skills: ${userData.skills}
+        Experience: ${userData.experience}
+        Write it professionally for a resume.
     `;
 
-    const response = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
+    const response = await groq.chat.completions.create({
+        model: "llama-3.1-8b-instant",
         messages: [{
             role:"user",
             content: prompt
-        }]
+        }],
+        temperature: 0.7
     })
 
     return response.choices[0].message.content;
