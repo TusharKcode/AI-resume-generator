83bf7580df919e8fc5cb1bf54117dc9c6b08de68
Tushar Kumar <tkgoyal104@gmail.com>
2026-03-12 18:04:15 +0530

    Route protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/auth/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
 import ResumeBuilder from "./pages/resume/ResumeBuilder";
 import './App.css'
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
@@ -12,8 +13,22 @@ function App() {
         <Route path="/" element={<Navigate to="/login"/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/resume-builder" element={<ResumeBuilder/>}/>
+        <Route
+          path="/dashboard" 
+          element={
+            <ProtectedRoutes>
+              <Dashboard/>
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/resume-builder" 
+          element={
+            <ProtectedRoutes>
+              <ResumeBuilder/>
+            </ProtectedRoutes>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
