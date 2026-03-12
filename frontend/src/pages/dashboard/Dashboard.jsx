2c85593acf86acaa7e8b54ae8411ada729954d7f
Tushar Kumar <tkgoyal104@gmail.com>
2026-03-12 18:14:37 +0530

    Dashboard working

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../services/authService.js";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logoutUser();
+    navigate("/login")
+  }
   return (
-    <div>Dashboard</div>
+    <div style={{padding:"30px"}}>
+      <h1>Welcome to AI Resume Generator Dashboard</h1>
+      <br />
+      <button onClick={() => navigate("/resume-builder")}>
+        Create new resume
+      </button>
+      <br />
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+      <br />
+    </div>
   )
 }
 
